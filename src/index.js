ff16593ef847a540e60866632d98ed25211444b5
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-01-24 22:09:00 -0500

    basic JS form handling
    added typewriter effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateHaiku(event) {
+  event.preventDefault();
 
+  new Typewriter("#haiku", {
+    strings: "rest light crimson demeanor",
+    autoStart: true,
+    delay: 100,
+    cursor: "🌿",
+  });
+}
+
+let inputFormElement = document.querySelector("#input-form");
+inputFormElement.addEventListener("submit", generateHaiku);
