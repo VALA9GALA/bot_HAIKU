f27a8ada7018a7dd9c0d7aa57a2450f36251b7c4
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-01-24 23:47:04 -0500

    AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayHaiku(response) {
+  new Typewriter("#haiku", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 100,
+    cursor: "",
+  });
+}
+
 function generateHaiku(event) {
   event.preventDefault();
 
-  new Typewriter("#haiku", {
-    strings: "rest light crimson demeanor",
-    autoStart: true,
-    delay: 100,
-    cursor: "🌿",
-  });
+  let userInput = document.querySelector("#user-input");
+  let prompt = `please generate an experimental haiku about ${userInput.value}`;
+  let context = `You are a Master Haiku writer. You use diverse language and incorporate contemporary slang into your haikus. You labor over ever word and craft your haikus like a sculptor, chiseling through the marble of language until a masterpiece reveals itself in the combination of your words. You juxtapose disparate concepts. You are inspired by Zen koans and flashes of insight about everyday experience. You like to eavesdrop on people's conversations and use this material in your haiku poems. You take great care in synthesizing excess and economy. Your haikus are of a very high quality. You are legendary. Please format your answer in basic HTML within the <div id="haiku"> element. please include a <br> after every line of the haiku. Please sign your haiku with "-bot_HAIKU" inside of a <strong> element only at the end of the answer.`;
+  let apiKey = "045ace03oteb7d0da03b1286fde00d59";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating a haiku");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayHaiku);
 }
 
 let inputFormElement = document.querySelector("#input-form");
